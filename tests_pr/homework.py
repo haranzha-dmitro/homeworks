0a958d077db3a32aa079905ac56077ddfd7e3fc4
haranzha-dmytro <dgaranzha@gmail.com>
2019-10-11 15:12:43 +0200

    homework.py, tests_unit.py tests_py.py

--- tests_pr/homework.py
+++ tests_pr/homework.py
@@ -1,6 +1,6 @@
-from math import (
-    log
-)
+import re
+import string
+from math import log
 
 
 def list_compare(list1, list2):
@@ -31,5 +31,95 @@ def push_zeros(numbers):
     non_zeros = [x for x in numbers if x != 0]
     return [*non_zeros, *zeros_list]
 
-def arithmetic_progres(numbers):
-    pass
+
+def arith_progress(numbers):
+    if len(numbers) < 2:
+        return False
+    arith_progress_step = numbers[1] - numbers[0]
+    ind = 1
+    while ind < len(numbers) - 1:
+        if numbers[ind + 1] != numbers[ind] + arith_progress_step:
+            return False
+        ind += 1
+    return True
+
+
+def number_not_twice(numbers):
+    return [element for element in numbers if list(numbers).count(element) == 1]
+
+
+def missing_number(numbers):
+    seq = range(min(numbers), max(numbers))
+    return [element for element in seq if element not in set(numbers)]
+
+
+def count_until_tuple(something):
+    result = 0
+    for element in something:
+        if type(element) == tuple:
+            break
+        result += 1
+    return result
+
+
+def reversed_text(text):
+    return str(text)[::-1]
+
+
+def hours_minutes(num):
+    return f"{num // 60}:{num - num // 60 * 60}"
+
+
+def first_longest_word(text):
+    return max([''.join([ch for ch in words if ch not in string.punctuation])
+                for words in text.split()], key=len)
+
+
+def words_in_backward():
+    return ' '.join(list(reversed(input().split())))
+
+
+def fibonnaci_seq():
+    fib1 = fib2 = 1
+    result = [fib1, fib2]
+    num = int(input("Enter how many Fibonnaci numbers to generate: ")) - 2
+    while num > 0:
+        fib1, fib2 = fib2, fib1 + fib2
+        num -= 1
+        result.append(fib2)
+    return result
+
+
+def list_with_even(numbers):
+    return [element for element in numbers if element % 2 == 0]
+
+
+def sum_of_num(num):
+    return sum(range(1, num + 1))
+
+
+def factorial(num):
+    if num == 0:
+        return 1
+    return num * factorial(num - 1)
+
+
+def letter_replace(text):
+    vowels = "aeiou"
+    res = "".join(string.ascii_lowercase[(string.ascii_lowercase.index(letter) + 1) %
+                                         len(string.ascii_lowercase)] for letter in text)
+    return ''.join(i.upper() if i in vowels else i for i in res)
+
+
+def sorted_letters(text):
+    return ''.join(sorted((letter for letter in text if letter in string.ascii_letters)))
+
+
+def two_nums(num1, num2):
+    if num1 == num2:
+        return -1
+    else:
+        return num1 < num2
+
+
+
--- tests_pr/tests_unit.py
+++ tests_pr/tests_unit.py
@@ -6,7 +6,16 @@ from homework import (
     power_of_three,
     sum_of_digits,
     push_zeros,
-    arithmetic_progres
+    arith_progress,
+    number_not_twice,
+    missing_number,
+    count_until_tuple,
+    reversed_text,
+    hours_minutes,
+    first_longest_word,
+    list_with_even,
+    sum_of_num,
+    factorial
 )
 
 
@@ -50,7 +59,49 @@ class TestsCase(unittest.TestCase):
 
     def test_arithmetic_progres(self):
         list1 = [5, 7, 9, 11]
-        self.assertTrue(arithmetic_progres(list1))
+        self.assertTrue(arith_progress(list1))
+
+    def test_number_not_twice(self):
+        testlist = [5, 3, 4, 3, 4]
+        result = [5]
+        self.assertEqual(number_not_twice(testlist), result)
+
+    def test_missing_number(self):
+        testlist = [1, 2, 3, 4, 6, 7, 8]
+        result = [5]
+        self.assertEqual(missing_number(testlist), result)
+
+    def test_count_until_tuple(self):
+        testlist = [1, 2, 3, (1, 2), 3]
+        result = 3
+        self.assertEqual(count_until_tuple(testlist), result)
+
+    def test_reversed_text(self):
+        text = "Hello World and Coders"
+        result = "sredoC dna dlroW olleH"
+        self.assertEqual(reversed_text(text), result)
+
+    def test_hours_minutes(self):
+        num = 13
+        result = "0:13"
+        self.assertEqual(hours_minutes(num), result)
+
+    def test_first_longest_word(self):
+        text = "fun&!! time"
+        result = "time"
+        self.assertEqual(first_longest_word(text), result)
+
+    def test_list_with_even(self):
+        numbers = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+        self.assertListEqual(list_with_even(numbers), [4, 16, 36, 64, 100])
+
+    def test_sum_of_num(self):
+        num = 4
+        self.assertEqual(sum_of_num(num), 10)
+
+    def test_factorial(self):
+        num = 4
+        self.assertEqual(factorial(num), 24)
 
 
 if __name__ == "__main__":
