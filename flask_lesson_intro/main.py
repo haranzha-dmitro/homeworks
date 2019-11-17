e3ac482d6931d312baa5a9c8675e6ade7508b82b
haranzha-dmytro <dgaranzha@gmail.com>
2019-11-17 18:56:51 +0200

    homework8 done

--- flask_lesson_intro/main.py
+++ flask_lesson_intro/main.py
@@ -8,6 +8,22 @@ app = Flask(__name__)
 def get_home_page():
     return render_template("home.html", data=get_data())
 
+@app.route('/<item>')
+def get_item_page(item):
+    for element in get_data():
+        if element['title'] == item:
+            print(item)
+            return render_template('item.html',
+                                   title=item,
+                                   item=item,
+                                   text=element['text'],
+                                   count=len(element['text']))
+    else:
+        return render_template("error404.html")
+
+@app.route('/author_page')
+def get_author_page():
+    return render_template('/author.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
