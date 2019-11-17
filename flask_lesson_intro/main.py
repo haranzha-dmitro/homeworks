a47ca12d390e744da7eb8b6f770792377f244e2a
haranzha-dmytro <dgaranzha@gmail.com>
2019-11-17 20:15:33 +0200

    main.py

--- flask_lesson_intro/main.py
+++ flask_lesson_intro/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_home_page():
-    return render_template("home.html")
+    return render_template("home.html", data=get_data())
 
 @app.route('/<item>')
 def get_item_page(item):
